2f487dbb2b0c16306141987f7d1b87aee7fe0030
Ioannis Gogos <Ioannis.Gogos@arhsdev.onmicrosoft.com>
2023-12-25 23:11:34 +0200

    Update app.module.ts with route configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  // more routes here
+];
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
