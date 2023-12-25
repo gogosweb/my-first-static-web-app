45508c9f42f7de4576c7f5bad16b8647a48b6906
Ioannis Gogos <Ioannis.Gogos@arhsdev.onmicrosoft.com>
2023-12-25 23:15:03 +0200

    Update package.json and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `
+    <div>Hello {{value}}</div>
+    <nav>
+      <ul>
+        <li><a routerLink="/">Home</a></li>
+        <li><a routerLink="/contact">Contact</a></li>
+        <li><a routerLink="/about">About</a></li>
+      </ul>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
   value = 'World';
